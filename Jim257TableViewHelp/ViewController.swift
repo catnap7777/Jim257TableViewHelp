f065ac294ef06b6003f7f9ccb1081e2acdfdb720
km <35552602+catnap7777@users.noreply.github.com>
2021-12-02 18:44:31 -0600

    Initial Commit

---
+++ Jim257TableViewHelp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jim257TableViewHelp
+//
+//  Created by Karen Mathes on 12/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
